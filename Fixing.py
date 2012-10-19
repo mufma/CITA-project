4c588f7bf694d1b7888ea153e057911e278f27a6
mufma <mufma@mail.ru>
2012-10-19 12:29:28 -0400

    fix

---
+++ Fixing.py
@@ -0,0 +1,14 @@
+prawn$ cd /mnt/raid-project/gmrt/eswitzer/
+prawn$ ls
+ls: .: Identifier removed
+prawn$ cd ..
+prawn$ ls
+ls: .: Identifier removed
+prawn$ cd ..
+prawn$ ls
+ls: reading directory .: Identifier removed
+prawn$ cd ..
+prawn$ ls
+data1	   data-pen2	    raid-cita	  scratch-3week  test
+data2	   data-pen3	    raid-project  scratch-pen
+data-pen1  gluster-scratch  scratch	  sunny-scratch
